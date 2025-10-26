f74e98f4f27559941a6a6d6e9552ca3401e0ade8
bao-123 <baomap040611vn@gmail.com>
2025-10-26 15:43:46 +0700

    Update FE

--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -1,8 +1,20 @@
+import styles from "../styles/home.module.css";
 
 function Home() {
     return (
         <>
-            <h1 className="text-blue-800">Hello world!</h1>
+            <div className={styles["container"]}>
+
+                <div className={styles["hero-content"]}>
+                    {/* Hero content here*/}
+                    <h1 className={styles["hero-header"]}>9A1</h1>
+                    <div className={styles["desc"]}>
+                        <h2>Trang web chính thức của lớp 9A1</h2>
+                        <p>Liên lạc - Lên kèo - Lưu giữ kỉ niệm</p>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
